c90dd2e139c34f1b88c9ad321602cdf194219edb
InColumi <max77648@gmail.com>
2023-03-26 22:48:13 +0300

    Added check

--- app/server/auth/database/connect.py
+++ app/server/auth/database/connect.py
@@ -1,10 +1,11 @@
 import psycopg2
+import json
 from config import Config
 
 
 class Excecutor:
     @staticmethod
-    def raw_excecute(sql_str: str):
+    def raw_excecute(sql_str: str, input_json: json = None):
         connection = None
         cursor = None
         output = {}
@@ -14,10 +15,16 @@ class Excecutor:
             connection = Excecutor.get_connect()
 
             cursor = connection.cursor()
-            cursor.execute(sql_str)
+
+            if input_json is not None:
+                cursor.execute(sql_str, (json.dumps(input_json), ))
+            else:
+                if sql_str.find("%s") != -1:
+                    raise Exception('Probably expected parameters, but json with parameters is not passed', sql_str)
+                cursor.execute(sql_str)
 
             data = cursor.fetchone()
-            output['data'] = data
+            output = data[0]
         except Exception as e:
             output['status'] = 1
             output["message"] = str(e)
@@ -26,6 +33,7 @@ class Excecutor:
             if cursor:
                 cursor.close()
             if connection:
+                connection.commit()
                 connection.close()
         return output
 
@@ -35,3 +43,34 @@ class Excecutor:
             return psycopg2.connect(**Config.database)
         except psycopg2.Error as e:
             raise Exception(f'Database connection error... Details: {e.pgerror}')
+
+    
+    def check_output(output):
+        """Check response from Database."""
+        if output is None:
+            raise ValueError("output=None")
+        if type(output) != dict:
+            raise Exception(f"Error: '{output}' to dict. Type: {type(output)}")
+        if output.get('status') is None:
+            raise Exception(f"Not key 'status' в '{output}'")
+        elif output.get('status') == 1 or output.get('status') == '1':
+            if output.get('message') is None:
+                raise Exception(f"Not key 'message' '{output}'")
+            raise Exception(output['message'])
+        text = str(output)
+        if text.find('None') != -1:
+            raise Exception("Пришел null из БД. Нужно обратиться к человеку, который писал метод."
+                            "(Эту проверку нужно убрать после тестирования).", output)
+
+    @staticmethod
+    def excecute_with_check(sql_str: str, input_json=None):
+        """Выполнение с проверкой."""
+        output = {}
+        output['status'] = 0
+        try:
+            output = Excecutor.raw_excecute(sql_str, input_json)
+            Excecutor.check_output(output=output)
+        except Exception as e:
+            output['status'] = 1
+            output['message'] = str(e)
+        return output
