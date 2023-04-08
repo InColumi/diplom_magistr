4ea15d56f67e273c3368a74f5e596ffd8967aa0e
InColumi <max77648@gmail.com>
2023-04-08 19:35:00 +0300

    Refactor

--- alembic/versions/307fb3a0741d_create_table_users.py
+++ alembic/versions/307fb3a0741d_create_table_users.py
@@ -1,7 +1,7 @@
 """Create table users
 
 Revision ID: 307fb3a0741d
-Revises: 
+Revises:
 Create Date: 2023-04-08 16:50:51.577803
 
 """
@@ -18,21 +18,17 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table('users' 
-    ,   Column(name='id', type_=Uuid, primary_key=True, default=uuid.uuid4())
-    ,   Column(name='email', type_=Text, unique=True, )
-    ,   Column(name='username', type_=Text, unique=True)
-    ,   Column(name='hashed_password', type_=Text, nullable=False)
-    ,   Column(name='salt', type_=Text, nullable=False)
-    ,   Column(name='is_deleted', type_=Boolean, nullable=False, default=False)
-    )
+    op.create_table('users',
+                    Column(name='id', type_=Uuid, primary_key=True, default=uuid.uuid4()),
+                    Column(name='email', type_=Text, unique=True, ),
+                    Column(name='username', type_=Text, unique=True),
+                    Column(name='hashed_password', type_=Text, nullable=False),
+                    Column(name='salt', type_=Text, nullable=False),
+                    Column(name='is_deleted', type_=Boolean, nullable=False, default=False))
 
-    op.create_check_constraint(
-        constraint_name='check_not_null_username_and_email'
-    ,   table_name='users'
-    ,   condition= 'username is not null or email is not null')
+    op.create_check_constraint(constraint_name='check_not_null_username_and_email', table_name='users', condition='username is not null or email is not null')
 
-    
+
 def downgrade() -> None:
     op.drop_table('users')
     pass
