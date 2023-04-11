199f46ac36b169d6e079b37fa07f24dc6f11c7e0
VladSolyony <solyonyvlad@gmail.com>
2023-04-11 21:50:47 +0300

    routes

--- app/client/src/index.tsx
+++ app/client/src/index.tsx
@@ -5,12 +5,15 @@ import './index.css'
 import store from './store/configureStore'
 import App from './App'
 import { ThemeProvider } from '@material-tailwind/react'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <Provider store={store}>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
-    </Provider>
+    <BrowserRouter>
+        <Provider store={store}>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </Provider>
+    </BrowserRouter>
 )
