fa5af63d6d33c8c1147e0553c2e7899d4fb92f11
InColumi <max77648@gmail.com>
2023-04-11 18:05:39 +0300

    Added new method

--- app/server/routers/auth.py
+++ app/server/routers/auth.py
@@ -1,23 +1,23 @@
-from dependencies import pwd_context, get_db, credentials_exception, settings
+from dependencies import pwd_context, get_db, credentials_exception, settings, has_access
 from datetime import datetime, timedelta
 from fastapi import APIRouter, Depends, HTTPException, Header
-from fastapi.security import OAuth2PasswordRequestForm, HTTPAuthorizationCredentials, HTTPBearer
+from fastapi.security import OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 from crud import crud_users
 from typing import Annotated
-from schemas.user import UserCreate
+from schemas.user import UserCreate, UserOut
 from schemas.token import Token
 from fastapi.encoders import jsonable_encoder
 
 router = APIRouter(tags=["auth"])
 
 
-def verify_password(plain_password, hashed_password):
+async def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def authenticate_user(db: Session, user_id: str, password: str):
+async def authenticate_user(db: Session, user_id: str, password: str):
     user = crud_users.get_user_by_id(db=db, user_id=user_id)
     if not user:
         return False
@@ -26,7 +26,7 @@ def authenticate_user(db: Session, user_id: str, password: str):
     return user
 
 
-def create_token(data: dict) -> Token:
+async def create_token(data: dict) -> Token:
     data_copy = data.copy()
     expiration_time = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     data_copy['exp'] = expiration_time
@@ -38,18 +38,25 @@ def create_token(data: dict) -> Token:
     return Token(access_token=access_token, refresh_token=refresh_token, expiration_time=settings.ACCESS_TOKEN_EXPIRE_MINUTES * 1000)
 
 
-def has_access(credentials: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
-    token = credentials.credentials
-    if not token:
-        raise credentials_exception
-    try:
-        jwt.decode(token, settings.ACCESS_TOKEN_SECRET_KEY, options={'require_exp': True})
-    except JWTError as e:
-        raise HTTPException(status_code=401, detail=str(e))
+async def get_current_user(data: Annotated[dict, Depends(has_access)], db: Session = Depends(get_db)) -> UserOut:
+    user_id = data['user_id']
+    db_user = crud_users.get_user_by_id(db, user_id)
+    return UserOut(id=db_user.id, email=db_user.email, username=db_user.username, is_deleted=db_user.is_deleted)
 
 
-@router.post('/refresh_token', dependencies=[Depends(has_access)], status_code=200)
-def refresh(token: str = Header(None)):
+async def get_active_current_user(user: Annotated[UserOut, Depends(get_current_user)]):
+    if user.is_deleted:
+        raise HTTPException(status_code=403, detail='User is not active.')
+    return user
+
+
+@router.get('/user/me', dependencies=[Depends(has_access)])
+def get_user_me(user: Annotated[UserOut, Depends(get_active_current_user)]):
+    return user
+    
+
+@router.post('/refresh_token', status_code=200)
+async def refresh(token: str = Depends(has_access)):
     try:
         data = jwt.decode(token, settings.REFRESH_TOKEN_SECRET_KEY)
         expiration_time = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
@@ -61,7 +68,7 @@ def refresh(token: str = Header(None)):
 
 
 @router.post('/sing_in')
-def sing_in(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
+async def sing_in(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
     db_user = crud_users.get_user_by_username(db, form_data.username)
     if not db_user:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
@@ -76,7 +83,7 @@ def sing_in(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Sess
 
 
 @router.put('/sign_up', status_code=200)
-def sign_up(user: UserCreate, db: Session = Depends(get_db)):
+async def sign_up(user: UserCreate, db: Session = Depends(get_db)):
     if user.email:
         db_user = crud_users.get_user_by_email(db, user.email)
         if db_user:
