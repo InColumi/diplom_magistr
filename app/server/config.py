03f5b19a1857f0ddf37cb3cd52b46da2c18498af
InColumi <max77648@gmail.com>
2023-04-12 15:52:33 +0300

    Changed new key

--- app/server/config.py
+++ app/server/config.py
@@ -1,15 +1,27 @@
 from pydantic import BaseSettings
-
+from typing import Union, Optional
+from datetime import timedelta
 
 class Settings(BaseSettings):
     CONNECT_STRING: str
     
-    ACCESS_TOKEN_SECRET_KEY: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFERSH_TOKEN_KEY: str
+    REFERSH_TOKEN_EXPIRES: int
+    ACCESS_TOKEN_KEY: str
+    ACCESS_TOKEN_EXPIRES: int
 
-    REFRESH_TOKEN_SECRET_KEY: str
-    REFRESH_TOKEN_EXPIRE_DAYS: int
 
+
+    # AUTHJWT_SECRET_KEY: str
+    # AUTHJWT_ACCESS_TOKEN_EXPIRES: Optional[int] = None
+    # AUTHJWT_REFRESH_TOKEN_EXPIRES: Optional[int] = None
+
+    # # cookies
+    # AUTHJWT_COOKIE_MAX_AGE: Optional[int] = None
+    # AUTHJWT_COOKIE_SECURE: Optional[bool] = False
+
+    
+    # AUTHJWT_REFRESH_COOKIE_KEY: str
     HOST: str
     PORT: int
 
