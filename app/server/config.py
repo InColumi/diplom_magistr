7d1e14b295fbde1043a55d568a2c6eddd6a74ed8
InColumi <max77648@gmail.com>
2023-04-29 21:31:46 +0300

    Added Path and extensions

--- app/server/config.py
+++ app/server/config.py
@@ -1,6 +1,4 @@
 from pydantic import BaseSettings
-from typing import Union, Optional
-from datetime import timedelta
 
 class Settings(BaseSettings):
     CONNECT_STRING: str
@@ -10,7 +8,8 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_KEY: str
     ACCESS_TOKEN_EXPIRES: int
 
-
+    PATH_BOOKS: str
+    EXTENSIONS_BOOKS: str
 
     # AUTHJWT_SECRET_KEY: str
     # AUTHJWT_ACCESS_TOKEN_EXPIRES: Optional[int] = None
