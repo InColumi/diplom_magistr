503cbe2d24df59c0ae8c6597febcf6111a808565
InColumi <max77648@gmail.com>
2023-05-01 18:15:33 +0300

    Added titles

---
+++ app/server/models/titles.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Uuid, Text, Integer
+from models.base import Base
+import uuid
+
+
+
+class Titles(Base):
+    __tablename__ = "titles"
+
+    id = Column(type_=Uuid, primary_key=True, default=uuid.uuid4())
+    name = Column(type_=Text, nullable=False)
+    ref_book_id = Column(type_=Uuid)
+    int_id = Column(type_=Integer, nullable=False)
+    int_book_id = Column(type_=Integer, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"Titles(id={self.id}, name={self.name}, ref_book_id={self.ref_book_id}, int_id={self.int_id}, int_book_id={self.int_book_id})"
