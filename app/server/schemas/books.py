c6cc2c389df37bae2d54c1e3cd11b07d870a8a49
InColumi <max77648@gmail.com>
2023-05-01 18:16:45 +0300

    Added new models

--- app/server/schemas/books.py
+++ app/server/schemas/books.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from datetime import date
 from uuid import UUID
+from typing import Union
 
 
 class Books(BaseModel):
@@ -9,5 +10,21 @@ class Books(BaseModel):
     int_id: int
 
 
-class Book(BaseModel):
+class BookIn(BaseModel):
     id: int
+    count_new_row: int
+
+
+class LimitPages(BaseModel):
+    pages: int
+
+
+class BookOut(BaseModel):
+    dateissued: date
+    name: str
+    authors: list
+    bookshelves_name: str
+    path_to_image: int
+    rating: int
+    is_favorite: Union[bool, None] = None
+    id: UUID
