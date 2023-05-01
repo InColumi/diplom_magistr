a520918d44c6eb557fd6e3c1e7fab76d4e4effe5
InColumi <max77648@gmail.com>
2023-05-01 18:15:23 +0300

    Added favorites

--- app/server/models/favorites.py
+++ app/server/models/favorites.py
@@ -1,12 +1,12 @@
 from sqlalchemy import Column, Uuid
-from sqlalchemy.orm import DeclarativeBase
+from models.base import Base
 
 
-class Favorites(DeclarativeBase):
+class Favorites(Base):
     __tablename__ = "favorites"
 
-    ref_users = Column(type_=Uuid, nullable=False)
-    ref_books = Column(type_=Uuid, nullable=False)
+    ref_users = Column(type_=Uuid, nullable=False, primary_key=True)
+    ref_books = Column(type_=Uuid, nullable=False, primary_key=True)
 
     def __repr__(self) -> str:
         return f"Favorites(ref_users={self.ref_users}, ref_books={self.ref_books})"
