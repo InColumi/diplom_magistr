c31be48ea5ee566b20080b9e2d52ffdb0df339ce
InColumi <max77648@gmail.com>
2023-05-01 18:15:00 +0300

    Added books

--- app/server/models/books.py
+++ app/server/models/books.py
@@ -1,13 +1,16 @@
-from sqlalchemy import Column, Uuid, Date, Integer
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, Uuid, Text, Integer
+from models.base import Base
 import uuid
 
-class Books(DeclarativeBase):
+
+class Books(Base):
     __tablename__ = "books"
 
     id = Column(type_=Uuid, primary_key=True, default=uuid.uuid4())
     int_id = Column(type_=Integer, nullable=False)
-    dateissured = Column(type_=Date, nullable=False)
+    bookshelves_id = Column(type_=Integer, nullable=False)
+    dateissued = Column(type_=Text, nullable=False)
+    rating = Column(type_=Integer, nullable=False, default=0)
 
     def __repr__(self) -> str:
-        return f"Books(id={self.id}, dateissured={self.dateissured})"
+        return f"Books(id={self.id}, int_id={self.int_id}, dateissued={self.dateissued}, bookshelves_id={self.bookshelves_id}, rating={self.rating})"
