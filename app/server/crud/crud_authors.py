aeae964f3045251eac10b8a3e7f71fc220f6581d
InColumi <max77648@gmail.com>
2023-05-08 09:01:48 +0300

    Changed top by readers

--- app/server/crud/crud_authors.py
+++ app/server/crud/crud_authors.py
@@ -1,4 +1,4 @@
-from models.books import Books
+from models.book_users import BookUsers
 from models.authors import Authors
 from sqlalchemy.orm import Session
 from sqlalchemy import select, func, Text
@@ -6,13 +6,16 @@ from models.book_authors import BookAuthors
 from sqlalchemy.dialects.postgresql import ARRAY
 
 
-def get_top_by_rating(db: Session, count_top: int):
-    authors_agg = func.array_agg(Authors.name, type_=ARRAY(Text)).label('authors')
-    query = select(Books.id, authors_agg)\
-        .join(BookAuthors, BookAuthors.ref_book_id == Books.int_id)\
-        .join(Authors, BookAuthors.ref_authors_id == Authors.int_id)\
-        .group_by(Books.id)\
-        .order_by(Books.rating_avg.desc())\
-        .limit(count_top)
+def get_top_by_readers(db: Session, count_top: int):
+	authors_agg = func.array_agg(Authors.name, type_=ARRAY(Text)).label('authors')
+	count_readers = select(func.count(BookUsers.ref_users).label('count_readers'))\
+                    .where(BookUsers.ref_books == BookAuthors.ref_book_id)\
+                    .scalar_subquery()
+	
+	query = select(BookAuthors.ref_authors_id, authors_agg, count_readers.label('count_readers'))\
+            .join(Authors, Authors.id == BookAuthors.ref_authors_id)\
+            .group_by(BookAuthors.ref_authors_id, BookAuthors.ref_book_id)\
+            .order_by(count_readers.desc())\
+            .limit(count_top)
 
-    return [item._asdict() for item in db.execute(query)]
+	return [item._asdict() for item in db.execute(query)]
