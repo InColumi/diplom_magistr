e86e21c2f562b9b710bdf5af0167e3581778f251
InColumi <max77648@gmail.com>
2023-05-29 21:32:00 +0300

    Added new tables

---
+++ app/server/models/test.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Text, Integer, ForeignKeyConstraint
+from models.base import Base
+
+
+class Test(Base):
+    __tablename__ = "test"
+
+    id = Column(type_=Integer, primary_key=True)
+    context = Column(type_=Text, nullable=False)
+    id_book = Column(type_=Integer, nullable=False)
+    centroid_id = Column(type_=Integer, nullable=False)
+    
+
+    def __repr__(self) -> str:
+        return f"Test(id={self.id}, \
+            context={self.context}, \
+            id_book={self.id_book}, \
+            centroid_id={self.centroid_id},)"
