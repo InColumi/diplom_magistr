afb1bf373abd8434c746ca7838db90901b11c658
InColumi <max77648@gmail.com>
2023-05-30 22:12:17 +0300

    Fix mistake

--- app/server/smart_search/model.py
+++ app/server/smart_search/model.py
@@ -22,17 +22,19 @@ def get_books_id_by_smart_search(db: Session, query: str) -> list:
         centroids.append(torch.Tensor(list(map(lambda i: float(i), centroid.split(',')))))
     res = get_clusters_list(query_embedding, centroids)
     
-    trashold = 0.3
+    trashold = 0.5
     for i in res[:5]:
         print(i)
     id_centoird_results = [i['corpus_id'] for i in res if i['score'] >= trashold] 
     id_centroid_db = [centroids_db_all[i].id for i in id_centoird_results]
     books_id = db.query(Test.id_book, Test.centroid_id).where(Test.centroid_id == any_(id_centroid_db)).group_by(Test.id_book, Test.centroid_id).all()
-
+    
     result = []
-    for i in books_id:
+    while len(id_centroid_db) > 0:
         centroid_id = id_centroid_db.pop(0)
-        if i[0] not in result:
-            if i[1] == centroid_id:
-                result.append(i[0])
+        for i in books_id:
+            if i[0] not in result:
+                if i[1] == centroid_id:
+                    result.append(i[0])
+    print(result)
     return result
